aeb8c099aa0c5e7901515d1005e2553d9c8293f9
yuipi7272 <yui20010218@gmail.com>
2021-08-30 14:32:03 +0000

    cssへの変更が表示されないので、herokuにデプロイする

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ end
 get '/' do
   count = Count.find(1)
   @number = count.number
+  count = Count.find(2)
   @number2 = count.number
   erb :index
 end
@@ -47,6 +48,41 @@ end
 
 post '/clear' do
   count = Count.find(1)
+  count.number = 0
+  count.save
+  redirect '/'
+end
+
+post '/plus2' do
+  count = Count.find(2)
+  count.number += 1
+  count.save
+  redirect '/'
+end
+
+post '/minus2' do
+  count = Count.find(2)
+  count.number -= 1
+  count.save
+  redirect '/'
+end
+
+post '/double2' do
+  count = Count.find(2)
+  count.number *= 2
+  count.save
+  redirect '/'
+end
+
+post '/divide2' do
+  count = Count.find(2)
+  count.number /= 2
+  count.save
+  redirect '/'
+end
+
+post '/clear2' do
+  count = Count.find(2)
   count.number = 0
   count.save
   redirect '/'
---
+++ db/migrate/20210830141557_title.rb
@@ -0,0 +1,5 @@
+class Title < ActiveRecord::Migration[6.1]
+  def change
+    add_column: :counts, :title, :string
+  end
+end
