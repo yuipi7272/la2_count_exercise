ddc38256dd4cc078bdfa9cebd2aa198f8a739c31
yuipi7272 <yui20010218@gmail.com>
2021-08-30 13:47:03 +0000

    Implement feature: creating new counter

--- app.rb
+++ app.rb
@@ -8,3 +8,44 @@ before do
     Count.create(number: 0)
   end
 end
+
+get '/' do
+  count = Count.find(1)
+  @number = count.number
+  erb :index
+end
+
+post '/plus' do
+  count = Count.find(1)
+  count.number += 1
+  count.save
+  redirect '/'
+end
+
+post '/minus' do
+  count = Count.find(1)
+  count.number -= 1
+  count.save
+  redirect '/'
+end
+
+post '/double' do
+  count = Count.find(1)
+  count.number *= 2
+  count.save
+  redirect '/'
+end
+
+post '/divide' do
+  count = Count.find(1)
+  count.number /= 2
+  count.save
+  redirect '/'
+end
+
+post '/clear' do
+  count = Count.find(1)
+  count.number = 0
+  count.save
+  redirect '/'
+end
