725a89a53c05557a834d0530adc144b98530fd27
yuipi7272 <yui20010218@gmail.com>
2021-08-31 04:42:45 +0000

    clear

--- app.rb
+++ app.rb
@@ -11,88 +11,44 @@ before do
 end
 
 get '/' do
-  count = Count.find(1)
-  @number = count.number
-  @title = count.title
-  
-  count = Count.find(2)
-  @number2 = count.number
-  @title2 = count.title
+  @counts = Count.all.order("created_at desc")
   erb :index
 end
 
-post '/title' do
-  count = Count.find(1)
-  count.title = params[:title]
-  count.save
+post '/create' do 
+  Count.create(number: 0, title: params[:title])
   redirect '/'
 end
-
-post '/plus' do
-  count = Count.find(1)
+post '/plus/:id' do
+  count = Count.find(params[:id])
   count.number += 1
   count.save
   redirect '/'
 end
 
-post '/minus' do
-  count = Count.find(1)
+post '/minus/:id' do
+  count = Count.find(params[:id])
   count.number -= 1
   count.save
   redirect '/'
 end
 
-post '/double' do
-  count = Count.find(1)
+post '/double/:id' do
+  count = Count.find(params[:id])
   count.number *= 2
   count.save
   redirect '/'
 end
 
-post '/divide' do
-  count = Count.find(1)
+post '/divide/:id' do
+  count = Count.find(params[:id])
   count.number /= 2
   count.save
   redirect '/'
 end
 
-post '/clear' do
-  count = Count.find(1)
-  count.number = 0
-  count.save
-  redirect '/'
-end
-
-post '/plus2' do
-  count = Count.find(2)
-  count.number += 1
-  count.save
-  redirect '/'
-end
-
-post '/minus2' do
-  count = Count.find(2)
-  count.number -= 1
-  count.save
-  redirect '/'
-end
-
-post '/double2' do
-  count = Count.find(2)
-  count.number *= 2
-  count.save
-  redirect '/'
-end
-
-post '/divide2' do
-  count = Count.find(2)
-  count.number /= 2
-  count.save
-  redirect '/'
-end
-
-post '/clear2' do
-  count = Count.find(2)
+post '/clear/:id' do
+  count = Count.find(params[:id])
   count.number = 0
   count.save
   redirect '/'
